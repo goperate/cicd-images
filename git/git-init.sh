cc203474175b11b1de1b6eef597f03b8e76cca45
pk <33803994+a1049990866@users.noreply.github.com>
2022-09-05 10:38:55 +0800

    Create git-init.sh

---
+++ git/git-init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+tee /etc/apt/sources.list <<-'EOF'
+deb http://mirrors.aliyun.com/debian/ bullseye main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ bullseye main non-free contrib
+deb http://mirrors.aliyun.com/debian-security/ bullseye-security main
+deb-src http://mirrors.aliyun.com/debian-security/ bullseye-security main
+deb http://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib
+deb http://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib
+EOF
+apt update && apt-get install expect -y
